b2d12e453dbe8aa7156cd84a20b53fdf71d96083
bneto@computacao.ufmt.cua.br <bneto@computacao.ufmt.cua.br>
2022-05-16 17:20:02 -0300

    feat: ativ 1, 2 3

--- m2/ativ1/Calendario.java
+++ m2/ativ1/Calendario.java
@@ -3,27 +3,31 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Calendario {
-    public static void main(String[] args) {
-      //Lê o mes e o ano
-      Scanner leitor = new Scanner(System.in);
-      System.out.printf("\nEntre com o ano: ");
-      int ano = leitor.nextInt();
-      System.out.printf("\nEntre com mês: ");
-      int mes = leitor.nextInt();
-      //definindo a data inicial
-      LocalDate data = LocalDate.of(ano, mes, 1);
-      //imprime o calendário
-      System.out.println("Dom Seg Ter Qua Qui Sex Sab");
+  public static void main(String[] args) {
+    //Lê o mes e o ano
+    Scanner leitor = new Scanner(System.in);
+    System.out.printf("\nEntre com o ano: ");
+    int ano = leitor.nextInt();
+    System.out.printf("\nEntre com mês: ");
+    int mes = leitor.nextInt();
+    //definindo a data inicial
+    LocalDate data = LocalDate.of(ano, mes, 1);
+    //imprime o calendário
+    imprimeCalendario(data,mes);
+  }
 
-      for(int i=0; i < (data.getDayOfWeek().getValue()); i++){
-        System.out.print("  ");
-      }
-
-      while(data.getMonthValue() == mes) {
-        System.out.printf("%2d ", data.getDayOfMonth());
-        if((data.getDayOfWeek().getValue()%7) == 0)
-          System.out.println();
-        data = data.plusDays(1); //avança um dia
+  public static void imprimeCalendario(LocalDate data, int mes){
+    System.out.println("Sun Mon Tues Wed Thu Fri Sat");
+    if(data.getDayOfWeek().getValue()!=7){
+      for(int i=1; i <= (data.getDayOfWeek().getValue()); i++){
+        System.out.print("    ");
       }
     }
+    while(data.getMonthValue() == mes) {
+      System.out.printf("%2d  ", data.getDayOfMonth());
+      if(data.getDayOfWeek().getValue()%6 == 0)
+        System.out.println();
+      data = data.plusDays(1); //avança um dia
+    }
+  }
 }
