c66e9d6ff283c1842b53af9a07c4703d8bc18e58
bneto@computacao.ufmt.cua.br <bneto@computacao.ufmt.cua.br>
2022-05-16 07:59:34 -0300

    feat: init module 2

---
+++ m2/ativ1/main.java
@@ -0,0 +1,29 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Calendario {
+    public static void main(String[] args) {
+      //Lê o mes e o ano
+      Scanner leitor = new Scanner(System.in);
+      System.out.printf("\nEntre com o ano: ");
+      int ano = leitor.nextInt();
+      System.out.printf("\nEntre com mês: ");
+      int mes = leitor.nextInt();
+      //definindo a data inicial
+      LocalDate data = LocalDate.of(ano, mes, 1);
+      //imprime o calendário
+      System.out.println("Seg Ter Qua Qui Sex Sab Dom");
+
+      for(int i=1; i < (data.getDayOfWeek().getValue()); i++){
+        System.out.print("  ");
+      }
+
+      while(data.getMonthValue() == mes) {
+        System.out.printf("%2d ", data.getDayOfMonth());
+        if((data.getDayOfWeek().getValue()%7) == 0)
+          System.out.println();
+        data = data.plusDays(1); //avança um dia
+      }
+    }
+}
